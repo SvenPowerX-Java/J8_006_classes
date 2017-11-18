d01f4015e454156f9050dda4f0cfd8643903a27e
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-18 17:32:43 +0100

    changed Box and create BoxDemo6 class

--- src/first_demo/Box.java
+++ src/first_demo/Box.java
@@ -9,6 +9,13 @@ public class Box {
 		return with * height * depth;
 	}
 	
+	public Box() {
+		System.out.println("Конструирование объекта Box");
+		this.with = 10;
+		this.height = 10;
+		this.depth = 10;
+	}
+	
 	void setDim(double with, double height, double depth) {
 		this.with = with;
 		this.height = height;
---
+++ src/first_demo/BoxDemo6.java
@@ -0,0 +1,10 @@
+package first_demo;
+
+public class BoxDemo6 {
+	public static void main(String[] args) {
+		Box mybox1 = new Box();
+		Box mybox2 = new Box();
+		System.out.println("Объм равен " + mybox1.volume());
+		System.out.println("Объм равен " + mybox2.volume());
+	}
+}
