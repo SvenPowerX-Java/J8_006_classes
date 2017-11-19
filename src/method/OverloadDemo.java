b414d907e9b6a39c518aaa0415dad4db06e7d733
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-19 12:26:14 +0100

    Modified OverloadDemo class and Added Overload class.

--- src/method/OverloadDemo.java
+++ src/method/OverloadDemo.java
@@ -5,19 +5,24 @@ package method;
  */
 public class OverloadDemo {
 	void test() {
-		System.out.println("Параметры отсутствуют");
+		System.out.println("    Параметры отсутствуют");
 	}
 	
 	void test(int a) {
-		System.out.println("a = " + a);
+		System.out.println("    a = " + a);
 	}
 	
 	void test(int a, int b) {
-		System.out.println("a = [" + a + "], b = [" + b + "]");
+		System.out.println("    a = " + a + ", b = " + b );
 	}
 	
 	double test(double a) {
-		System.out.println("a = [" + a + "]");
+		System.out.println("    a = " + a);
 		return a * a;
 	}
+	double test2(double a) {
+		System.out.println("    a = " + a);
+		return a * a;
+	}
+	
 }
