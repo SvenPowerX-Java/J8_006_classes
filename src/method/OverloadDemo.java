3d9140bfb4cd17d2cbd522ac2b2c088fa193c47b
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-19 11:55:07 +0100

    Added method package and OverloadDemo class.

---
+++ src/method/OverloadDemo.java
@@ -0,0 +1,23 @@
+package method;
+
+/**
+ * Перегрузка методов
+ */
+public class OverloadDemo {
+	void test() {
+		System.out.println("Параметры отсутствуют");
+	}
+	
+	void test(int a) {
+		System.out.println("a = " + a);
+	}
+	
+	void test(int a, int b) {
+		System.out.println("a = [" + a + "], b = [" + b + "]");
+	}
+	
+	double test(double a) {
+		System.out.println("a = [" + a + "]");
+		return a * a;
+	}
+}
