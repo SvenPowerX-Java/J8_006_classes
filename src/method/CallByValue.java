3d888cd7311d1a6710016e14e8d2b1ad7146adc0
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-19 18:02:16 +0100

    Modified Test class and CallByValue, PassObjRe, RetOb class.

---
+++ src/method/CallByValue.java
@@ -0,0 +1,13 @@
+package method;
+
+public class CallByValue {
+	public static void main(String[] args) {
+		Test ob = new Test();
+		
+		int a = 15, b = 20;
+		
+		System.out.println("a и b до вызова: " + a + " " + b);
+		ob.meth(a, b);
+		System.out.println("a и b после вызова: " + a + " " + b);
+	}
+}
--- src/method/Test.java
+++ src/method/Test.java
@@ -3,6 +3,13 @@ package method;
 public class Test {
 	int a, b;
 	
+	public Test() {
+	}
+	
+	public Test(int i) {
+		a = i;
+	}
+	
 	public Test(int a, int b) {
 		this.a = a;
 		this.b = b;
@@ -11,5 +18,19 @@ public class Test {
 	public boolean equals(Test o) {
 		if (o.a == a && o.b == b) return true;
 		else return false;
+	}
+	
+	void meth(int i, int j) {
+		i *= 2;
+		j /= 2;
+	}
+	
+	void meth(Test o) {
+		o.a *= 2;
+		o.b /= 2;
+	}
+	Test incrByTen() {
+		Test temp = new Test(a + 10);
+		return temp;
 	}
 }
