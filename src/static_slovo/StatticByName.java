edaba03b43ce8f671bb1c9c8dc3e595d04a963ad
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-19 19:55:11 +0100

    Added classes static

---
+++ src/static_slovo/StatticByName.java
@@ -0,0 +1,8 @@
+package static_slovo;
+
+public class StatticByName {
+	public static void main(String[] args) {
+		StaticDemo.callMe();
+		System.out.println("StaticDemo.b = " + StaticDemo.b);
+	}
+}
