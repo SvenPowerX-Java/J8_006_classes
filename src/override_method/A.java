9a86922e1b4be5b4c061699e06eb16b86f798860
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-11-20 23:09:06 +0100

    Added package override_method and A class

---
+++ src/override_method/A.java
@@ -0,0 +1,13 @@
+package override_method;
+
+public class A {
+	int i, j;
+	
+	public A(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	void show() {
+		System.out.println("i and j: " + i+" "+j);
+	}
+}
